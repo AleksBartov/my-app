376c279ed4e7e774c97912ace8a5532b879acee0
Aleksandr <aleksandrbartov@yandex.ru>
2020-01-04 02:23:05 +0300

    changing folder's name

---
+++ docs/precache-manifest.a59c877b3a8c37ccf5d38e0daaa915b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24ed21184b4453feb0c8baf95eec2bcd",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "c41158a3d0dda9d49986",
+    "url": "/my-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d43b94ca10b18c919223",
+    "url": "/my-app/static/js/2.17fd5311.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/my-app/static/js/2.17fd5311.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c41158a3d0dda9d49986",
+    "url": "/my-app/static/js/main.7273651c.chunk.js"
+  },
+  {
+    "revision": "1d0d1868f76663eec549",
+    "url": "/my-app/static/js/runtime-main.467216da.js"
+  }
+]);
